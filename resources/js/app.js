20d74c9e5709d4b26fad03f42afbe3d802541606
Criafge <110690865+criafge@users.noreply.github.com>
2024-02-27 17:43:22 +0500

    controller and components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import VueRouter from 'vue-router';
+use.createApp(VueRouter)
+
+const app = createApp({});
+
+import AppComponent from './components/AppComponent.vue'
+app.component('app-component', AppComponent);
+
+app.mount('#app');
